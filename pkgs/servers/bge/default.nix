2f9a7ca4861813f68759d164117e362165a4b4bd
Jorge and Stefan <info@bitcoinprivacy.net>
2016-09-06 14:04:58 +0200

    Fetch the code from the last Stable revision

--- pkgs/servers/bge/default.nix
+++ pkgs/servers/bge/default.nix
@@ -29,8 +29,8 @@
 
     src = fetchgit {
     url = "git://github.com/bitcoinprivacy/Bitcoin-Graph-Explorer.git";
-    rev = "HEAD";
-    md5 = "e1eb9d80ac6ded2c5a097eb3dd212379";
+    rev = "d309d06";
+    md5 = "6735bcb2a2d6597e568ad50eb08459a9";
     } ;
     JAVA_HOME = "${jdk}";
     shellHook = ''
